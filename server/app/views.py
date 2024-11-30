9d9d76e2409c0e637d645e36c0d1d0baa02dcf89
Michael Koenig <koenigmichael2002@gmail.com>
2024-11-30 16:08:12 +0100

    #10 Add Browse Words page

--- server/app/views.py
+++ server/app/views.py
@@ -1,12 +1,25 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import os
+from django.conf import settings
 
 
 def index(request):
     return render(request, 'app/index.html')
 
+
 def browse(request):
-    return render(request, 'app/browse.html')
+    video_dir = os.path.join(settings.STATICFILES_DIRS[0], 'videos')
+    # Get a list of video file names
+    video_files = [
+        {
+            'path': f"videos/{file}",  # File path for the video
+            'name': os.path.splitext(file)[0]  # File name without extension
+        }
+        for file in os.listdir(video_dir) if file.endswith(('.mp4', '.webm', '.ogg'))
+    ]
+    return render(request, 'app/browse.html', {'video_files': video_files})
+
 
 def study(request):
     return render(request, 'app/study.html')
