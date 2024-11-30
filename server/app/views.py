aa8eb74fd15b70f08808dd854d8c969f7e565d16
Michael Koenig <koenigmichael2002@gmail.com>
2024-11-30 15:16:31 +0100

    #10 Add bulma CSS library

--- server/app/views.py
+++ server/app/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello ASL learner! This is our landing page!")
+    return render(request, 'app/index.html')
