5a3460dc83714497a94253e7856438d36246e4a7
parisab <parisab@student.chalmers.se>
2024-12-02 17:37:24 +0100

    #16 Add visualization option to landmark detection
    Co-authored-by:  @faundez

--- model_training/landmark_detector.py
+++ model_training/landmark_detector.py
@@ -3,6 +3,7 @@
 
 
 import mediapipe as mp
+from mediapipe.framework.formats import landmark_pb2
 import os
 import cv2 as cv
 
@@ -18,23 +19,34 @@ def get_detector():
     options = mp.tasks.vision.HandLandmarkerOptions(base_options=base_options, num_hands=2)
     return mp.tasks.vision.HandLandmarker.create_from_options(options)
 
-def get_landmarks(video_path, detector):
+def get_landmarks(video_path, detector, show_landmarks=False):
     cap = cv.VideoCapture(video_path)
-    landmarks = []
+    all_landmarks = []
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             break
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
         detection_result = detector.detect(mp_image)
-        hand_landmarks_list = detection_result.hand_landmarks
-        landmarks.append(hand_landmarks_list)
+        hand_landmarks = detection_result.hand_landmarks
+        all_landmarks.append(hand_landmarks)
+        if show_landmarks:
+            if hand_landmarks:
+                normal_list = landmark_pb2.NormalizedLandmarkList()
+                normal_list.landmark.extend([
+                landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks[0]
+                ])
+                mp.solutions.drawing_utils.draw_landmarks(frame, normal_list, mp.solutions.hands.HAND_CONNECTIONS)
+                cv.imshow('Hand Landmarks', frame)
+            if cv.waitKey(1000) & 0xFF == ord('q'):
+                break
     cap.release()
-    return landmarks
+    cv.destroyAllWindows()
+    return all_landmarks
 
 
 if __name__ == '__main__':
     detector = get_detector()
     video_path = 'preprocessing/dataset/train/like/0005.mp4'
-    landmarks = get_landmarks(video_path, detector)
+    landmarks = get_landmarks(video_path, detector, True)
     print(landmarks)
