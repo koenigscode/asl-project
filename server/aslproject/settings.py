bd5f4df3fa3987f361c486aa58213392b682ec72
serbina <serbina@net.chalmers.se>
2025-01-03 19:47:08 +0200

    #14 Try to fix locks on db

--- server/aslproject/settings.py
+++ server/aslproject/settings.py
@@ -86,6 +86,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'OPTIONS': {
+            'timeout': 20,
+            'journal_mode': 'WAL',
+        },
     }
 }
 
